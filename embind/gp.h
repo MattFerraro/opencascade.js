55cf3aedc515745ca8e8102f4568ec662a7fc576
Sebastian Alff <donalffons@gmail.com>
2020-08-24 21:39:15 +0200

    nicer formatting

--- embind/gp.h
+++ embind/gp.h
@@ -1,10 +1,7 @@
 // gp_Pnt
 class_<gp_Pnt>("gp_Pnt")
   .function("Coord_1", select_overload<Standard_Real(const Standard_Integer)const>(&gp_Pnt::Coord))
-  .function(
-    "Coord_2", reinterpret_cast<
-      void (gp_Pnt::*)(const Standard_Real&, const Standard_Real&, const Standard_Real&)const
-    > ((
-      void (gp_Pnt::*)(Standard_Real&, Standard_Real&, Standard_Real&)const
-    ) &gp_Pnt::Coord)
+  .function("Coord_2",
+    reinterpret_cast<void (gp_Pnt::*)(const Standard_Real&, const Standard_Real&, const Standard_Real&)const>
+    (select_overload<void(Standard_Real&, Standard_Real&, Standard_Real&)const>(&gp_Pnt::Coord))
   );
