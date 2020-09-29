a21f548fecd6830850d83f21a7bc934b2c1be95a
Sebastian Alff <donalffons@gmail.com>
2020-09-29 09:20:26 +0200

    downgrade to occtv7.3.0p3

--- make.py
+++ make.py
@@ -144,7 +144,7 @@ def build():
   targetfile = "occt.tar.gz"
   if not os.path.exists(targetfile):
     stage("downloading OCCT...")
-    url = "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=33d9a6fa21ca4fa711da7066655aa2ba854545ee;sf=tgz"
+    url = "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=0858125fd4ef81ca2965e1aa2f49c5960d7dcc62;sf=tgz"
     myfile = requests.get(url, stream=True)
     open(targetfile, 'wb').write(myfile.content)
 
